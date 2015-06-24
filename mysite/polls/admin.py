2d106dbf12c7b6a54e6dd48a49c7004327203ad9
Yuriy Zaliznyuk <yuriy.zaliznyuk@gmail.com>
2015-06-24 19:10:28 +0300

    First re-factored commit

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -6,8 +6,7 @@ class ChoiceInline(admin.TabularInline):
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'pub_date')
-    #list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
